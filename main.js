93aacc47a6498839dbbc67a3590579a3e280a8ea
lhays <lhays9606@gmail.com>
2024-12-27 15:51:03 -0500

    clean up

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
 import { chromium } from '@playwright/test';
+import { argv } from 'node:process'
 import fs from 'fs';
 import path from 'path';
 
-const BASE_URL = 'https://treecommerce.dev';
-const PATH = '/products/the-mystery-liquid';
-const LOCAL_MAPS_DIR = './maps';
-const __dirname = path.resolve();
+const args = argv.slice(2)
+
+if (args <= 2) {
+  console.error('Incorrect number of arguments provided.\nFormat should be "node main.js <url>"')
+  process.exit(1)
+}
+
+const URL = args[0]
+const LOCAL_MAPS_DIR = './maps'
 
 const browser = await chromium.launch();
 const page = await browser.newPage();
 
 const sourceMapRequests = [];
 
-// Set up response listener
+// Response listener will parse each response and check for a sourceMappingURL that matches to a .map file
+// This source map will be fetched and processed later
 page.on('response', async (response) => {
   const url = response.url();
 
@@ -22,8 +29,8 @@ page.on('response', async (response) => {
   const sourceMapUrl = body.match(/sourceMappingURL=([^'"]+\.map)/)?.[1];
 
   if (sourceMapUrl) {
-    console.log(`Response with source map URL: ${url}`);
-    console.log(`Source map URL: ${sourceMapUrl}`);
+    console.log(`Response with source map URL in chunk: ${url}`);
+    console.log(`Fetching source map: ${sourceMapUrl} ...`);
 
     sourceMapRequests.push(
       new Promise((resolve) => {
@@ -35,28 +42,27 @@ page.on('response', async (response) => {
   console.log('--------------------------------');
 });
 
-// Go to the page we are interested in attempt to view src maps on
-await page.goto(`${BASE_URL}${PATH}`);
+await page.goto(URL);
+
 // Add a small delay to ensure we capture all responses
 await page.waitForLoadState('networkidle');
-// Wait for all requests for source map files to finish
-const results = await Promise.allSettled(sourceMapRequests);
+const sourceMapResults = await Promise.allSettled(sourceMapRequests);
 
 // Create a directory for maps if it does not already exist
-fs.mkdir(path.resolve(__dirname, LOCAL_MAPS_DIR), {recursive: true}, e => {
+fs.mkdir(LOCAL_MAPS_DIR, {recursive: true}, e => {
   if (e) {
     console.error(e);
-    return;
+    process.exit(1);
   }
 })
 
 // Go through each response to fetch the map file
-results.forEach(async result => {
+sourceMapResults.forEach(async result => {
   const response = result.value;
   const body = await response.text();
 
   const fileName = response.url().split('/').pop()
-  fs.writeFile(`${__dirname}\\maps\\${fileName}`, body, err => {
+  fs.writeFile(`${path.resolve(LOCAL_MAPS_DIR, fileName)}`, body, err => {
     if (err) {
       console.error(err)
     }
@@ -65,7 +71,7 @@ results.forEach(async result => {
   const encodedSourceMap = body.match(/sourceMappingURL=data:application\/json;.*;base64,([^"]+)/)?.[1];
   if (encodedSourceMap) {
     const decodedSourceMap = Buffer.from(encodedSourceMap, 'base64').toString('utf-8');
-    fs.writeFile(`${__dirname}\\maps\\${fileName}.out.map`, decodedSourceMap, err => {
+    fs.writeFile(`${path.resolve(LOCAL_MAPS_DIR, fileName+'.decoded.map')}`, decodedSourceMap, err => {
       if (err) {
         console.error(err)
       }
